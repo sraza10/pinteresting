5c204fd3b413a51a35e5685678f97b4cfe10b6a8
Syed Raza <syedaraza10@gmail.com>
2013-12-17 00:35:41 -0500

    Added sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "resources" => "pages#resources"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
