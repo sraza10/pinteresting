e4256d0f97821f60d50f945fd1f791c73226028f
Syed Raza <syedaraza10@gmail.com>
2013-12-18 15:40:12 -0500

    implemented devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Pinteresting::Application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about"
-  get "resources" => "pages#resources"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
